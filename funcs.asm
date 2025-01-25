4bced00ff854e63ffeb108f0eff8039ff79623a3
Mayoki_mano <dima-steam008@Mail.ru>
2025-01-25 20:49:27 +0300

    Ускорена работа ассемблерной вставки asm_152, скорость 157 мб/с

--- funcs.asm
+++ funcs.asm
@@ -10,20 +10,51 @@ global kyznechick_asm_fast
 global kyznechick_asm_133
 
 extern LS_mat               ; Объявляем глобальный массив LS_mat
-
+; used- r9,r11,r8,rcx,rax
+; unused - r10, rdx, rcx(?)
+; требуют восстановления, но в принципе можно использовать rbx, rbp(?), rdi, rsi, r12, r13, r14, r15
 ; Arguments:
 ;   rcx - round_keys (chunk *) - a pointer to the round keys
 ;   rdx - in (chunk) - a pointer to the input data
 ;   r8 - out (chunk) - a pointer to the output data
 kyznechick_asm_152:
+    push r12
+    push r13
+    push r14
+    push r15
+    push rbx
+    push rbp
+    push rsi
+    push rdi
     ; Загрузка входных данных (16 байт)
     movaps xmm0, [rdx]      ; xmm0 <- in
     mov r11, LS_mat         ; r11 <- LS_mat
+    mov r10, r11            ; r10 <- LS_mat
+    add r11, 4096             ; r11 <- LS_mat[1]
+    mov rdx, r11            ; rdx <- LS_mat[1]
+    add r11, 4096             ; r11 <- LS_mat[2]
+    mov r12, r11            ; r12 <- LS_mat[2]
+    add r11, 4096             ; r11 <- LS_mat[3]
+    mov r13, r11            ; r13 <- LS_mat[3]
+    add r11, 4096             ; r11 <- LS_mat[4]
+    mov r14, r11            ; r14 <- LS_mat[4]
+    add r11, 4096             ; r11 <- LS_mat[5]
+    mov r15, r11            ; r15 <- LS_mat[5]
+    add r11, 4096             ; r11 <- LS_mat[6]
+    mov rsi, r11            ; rsi <- LS_mat[6]
+    add r11, 4096             ; r11 <- LS_mat[7]
+    mov rdi, r11            ; rdi <- LS_mat[7]
+    add r11, 4096             ; r11 <- LS_mat[8]
+    mov rbp, r11            ; rbp <- LS_mat[8]
+    add r11, 4096             ; r11 <- LS_mat[9]
+    mov rbx, r11            ; rbx <- LS_mat[9]
+    add r11, 4096             ; r11 <- LS_mat[10]
     lea r9, [rel mask]      ; r9 <- &mask
     movaps xmm5, [r9]       ; xmm5 <- mask
     mov r9, rcx             ; r9 <- указатель на round_keys
     ; Основной цикл раундов (9 итераций)
     mov ecx, 9              ; Количество раундов (9 для "Кузнечика")
+
 
 round_loop:
     ; Применение раундового ключа
@@ -41,56 +72,46 @@ round_loop:
     ; Обработка всех 16 байтов (развёрнутый цикл)
     ; Байт 0 (чётный)
     pextrw eax, xmm2, 0      ; eax <- in_out[0] * 16
-    movaps xmm0, [r11 + rax]         ; xmm0 <- xmm0 ^ LS_mat[0][in_out[0]]
+    movaps xmm0, [r10 + rax]         ; xmm0 <- xmm0 ^ LS_mat[0][in_out[0]]
 
     ; Байт 1 (нечётный)
     pextrw eax, xmm3, 0      ; eax <- in_out[1] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[1][in_out[1]]
+    pxor xmm0, [rdx+rax]         ; xmm0 <- xmm0 ^ LS_mat[1][in_out[1]]
 
     ; Байт 2 (чётный)
     pextrw eax, xmm2, 1      ; eax <- in_out[2] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[2][in_out[2]]
+    pxor xmm0, [r12+rax]         ; xmm0 <- xmm0 ^ LS_mat[2][in_out[2]]
 
     ; Байт 3 (нечётный)
     pextrw eax, xmm3, 1      ; eax <- in_out[3] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[3][in_out[3]]
+    pxor xmm0, [r13+rax]         ; xmm0 <- xmm0 ^ LS_mat[3][in_out[3]]
 
     ; Байт 4 (чётный)
     pextrw eax, xmm2, 2      ; eax <- in_out[4] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[4][in_out[4]]
+    pxor xmm0, [r14+rax]         ; xmm0 <- xmm0 ^ LS_mat[4][in_out[4]]
 
     ; Байт 5 (нечётный)
     pextrw eax, xmm3, 2      ; eax <- in_out[5] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[5][in_out[5]]
+    pxor xmm0, [r15+rax]         ; xmm0 <- xmm0 ^ LS_mat[5][in_out[5]]
 
     ; Байт 6 (чётный)
     pextrw eax, xmm2, 3      ; eax <- in_out[6] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[6][in_out[6]]
+    pxor xmm0, [rsi+rax]         ; xmm0 <- xmm0 ^ LS_mat[6][in_out[6]]
 
     ; Байт 7 (нечётный)
     pextrw eax, xmm3, 3      ; eax <- in_out[7] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[7][in_out[7]]
+    pxor xmm0, [rdi+rax]         ; xmm0 <- xmm0 ^ LS_mat[7][in_out[7]]
 
     ; Байт 8 (чётный)
     pextrw eax, xmm2, 4      ; eax <- in_out[8] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[8][in_out[8]]
+    pxor xmm0, [rbp+rax]         ; xmm0 <- xmm0 ^ LS_mat[8][in_out[8]]
 
     ; Байт 9 (нечётный)
     pextrw eax, xmm3, 4      ; eax <- in_out[9] * 16
-    add r11, 4096
-    pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[9][in_out[9]]
+    pxor xmm0, [rbx+rax]         ; xmm0 <- xmm0 ^ LS_mat[9][in_out[9]]
 
     ; Байт 10 (чётный)
     pextrw eax, xmm2, 5      ; eax <- in_out[10] * 16
-    add r11, 4096
     pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[10][in_out[10]]
 
     ; Байт 11 (нечётный)
@@ -118,7 +139,7 @@ round_loop:
     add r11, 4096
     pxor xmm0, [r11+rax]         ; xmm0 <- xmm0 ^ LS_mat[15][in_out[15]]
 
-    sub r11, 61440          ; r11 <- LS_mat
+    sub r11, 20480          ; r11 <- LS_mat
     ; Следующий раунд
     dec ecx                 ; Уменьшаем счётчик раундов
     jnz round_loop          ; Если ecx != 0, продолжаем цикл
@@ -126,7 +147,14 @@ round_loop:
     ; Финальный раунд (последний XOR с раундовым ключом)
     pxor xmm0, [r9]         ; xmm0 <- in ^ round_keys[9]
     movaps [r8], xmm0      ; Сохранение результата в out
-
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rbx
+    pop r15
+    pop r14
+    pop r13
+    pop r12
     ret
 
 ; Arguments:
@@ -137,7 +165,6 @@ kyznechick_asm_133:
     ; Загрузка входных данных (16 байт)
     movdqa xmm1, [rdx]      ; xmm1 <- in (16 байт)
     mov r11, LS_mat          ; r11 <- указатель на LUT-таблицу LS_mat
-
     ; Основной цикл раундов
     mov r9, rcx              ; r9 <- указатель на round_keys
     mov ecx, 9               ; Количество раундов (9 для "Кузнечика")
